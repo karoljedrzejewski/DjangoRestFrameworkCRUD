83f77176cc5518c26f12a401839f9ba02d9b9e08
Karol Jędrzejewski <karol.jedrzejewski10@gmail.com>
2023-06-12 23:39:45 +0200

    production.py

--- djangoAPI/settings.py
+++ djangoAPI/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '37hfjzc2xwp%_^^))ou36fsx)3fl@ix+0n2a7!zo%!-gk*^3@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [djangoapibasic.azurewebsites.net]
+ALLOWED_HOSTS = ['djangoapibasic.azurewebsites.net']
 
 
 
